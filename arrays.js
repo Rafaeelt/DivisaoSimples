7bd42542223daf8938b5e394a38dded4d501cf35
𝖱𝖺𝖿𝖺𝖾𝗅 <rafael.oliveira.silva09@escola.pr.gov.br>
2026-05-04 09:22:46 -0300

    Add files via upload

---
+++ arrays.js
@@ -0,0 +1,15 @@
+const lojaA =[
+    {nome: "Samsung A14", preco:900},
+    {nome: "Moto G54", preco:1200},
+    {nome: "IPhone 11", preco:2500},
+    {nome: "IPhone 13", preco:6000},
+];
+const lojaB =[
+    {nome: "Xaiomi Note 12", preco:1000},
+    {nome: "Poco X5", preco:1300},
+    {nome: "Realme 11", preco:1500},
+    {nome: "IPhone 13", preco:3500},
+    {nome: "IPhone 14", preco:4500},
+    {nome: "Asus ROG Phone", preco:5000},
+];
+module.exports = {lojaA, LojaB};
---
+++ index.js
@@ -0,0 +1,12 @@
+const {lojaA, LojaB} = require('./arrays');
+
+function juntaListas (lista1,lista2){
+  let listaFinal = [];
+  let posicaoAtualLista1= 0;
+  let posicaoAtualLista2= 0;
+  let atual = 0; 
+
+
+}
+
+console.log{juntaListas(lojaA,LojaB)};
